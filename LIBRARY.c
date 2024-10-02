d633e44459a43d145161591a8d936ce1bba98fd6
DYLAN4204 <kigendylan4204@gmail.com>
2024-10-02 17:56:21 +0300

    Add files via upload

---
+++ LIBRARY.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+/* autor:Dylan kigen 
+reg No:CT101/G/21726/24 
+*/
+
+int main()
+{
+    int book_id;
+    int due_date;
+    int return_date;
+    int over_due_date;
+    
+    //prompt the user to enter the book _id
+    printf("enter the book id:");
+    scanf("%d",&book_id);
+    //prompt the user to enter the due date
+    printf("enter the due date:");
+    scanf("%d",&due_date);
+    //prompt the user to enter the return_date 
+    printf("enter the return date:");
+    scanf("%d",&return_date);
+    //calculate the over_due date
+    over_due_date=due_date-return_date;
+    if(over_due_date<=7){
+    int fine_rate=20;
+    int fine_amount=fine_rate*over_due_date;
+    printf("the fine amount is:%2d",fine_amount);
+    
+    }
+    else if(over_due_date<15){
+    int fine_rate=50;
+    int fine_amount=fine_rate*over_due_date;
+    printf("the fine amount is:%2d",fine_amount);
+    }
+    else {
+        int fine_rate=100;
+        int fine_amount=fine_rate*over_due_date;
+        printf("the fine amount is:%2d",fine_amount);
+        }
+        printf("\nthe book_id is:%d",book_id);
+        printf("\nthe duedate is :%d",due_date);
+        printf("\nthe returndate is :%d", return_date);
+        printf("\nthe over due date is :%d",over_due_date);
+        
+        
+        return 0;
+        }
+    
+    
+    
---
+++ SIMPLE INTEREST .c
@@ -0,0 +1,22 @@
+//program to find the simple interest 
+
+/*Author:Kigen Dylan
+date 17/09/2021
+*/
+#include <stdio.h>
+
+int main()
+{
+int a,b,c ;
+int simpleinterest;
+printf("enter the principal amount:");
+scanf("%d",&a);
+printf("enter the time (in years):");
+scanf("%d",&b);
+printf("enter the rate in(%%):");
+scanf("%d",&c);
+ simpleinterest=(a*b*c)/100;
+printf("the simple interest is:%d, simple interest");
+return 0;
+}
+
