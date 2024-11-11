697d5e33f63a798b7978fb75e1a3fa4c90917f09
DYLAN4204 <kigendylan4204@gmail.com>
2024-11-11 11:47:04 +0300

    Add files via upload

---
+++ EVEN .c
@@ -0,0 +1,21 @@
+//program to very if the numbers is even number
+#include <stdio.h>
+
+void checkEvenOrOdd(int num) {
+    if (num % 2 == 0) {
+        printf("The number is even\n");
+    } else {
+        printf("The number is odd\n");
+    }
+}
+
+int main() {
+    int number;
+
+    printf("Enter a number: ");
+    scanf("%d", &number);
+
+    checkEvenOrOdd(number);
+
+    return 0;
+    }
